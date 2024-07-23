2b44f72dd5648336017cf920434f41fac4cabf38
lexag <lexa.grebnets@gmail.com>
2024-07-23 14:24:07 +0200

    instance track segment (untested)

--- source/WorldManager.cs
+++ source/WorldManager.cs
@@ -1,12 +1,27 @@
 using Godot;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
 
 public enum WorldManagerMode
 {
 	World,
 	Menu
+}
+
+public struct TrackPoint
+{
+	public double xoffset;
+	public double yoffset;
+	public Dictionary<ulong, TrackPoint> linked_nodes;
+	public float[] tangent;
+}
+
+public struct TrackPointLink
+{
+	public Vector2 direction;
+	public float distance;
+	public Dictionary<string, string> tags;
 }
 
 public static class WorldManager
@@ -15,19 +30,32 @@ public static class WorldManager
 
 	public static void Setup()
 	{
-		VehicleManager.vehicleWorldCoordinate = RouteManager.trackNodesInRoute.Last().Value.WorldCoordinate;
-		WorldRenderer.RenderListOfTrackNodes(RouteManager.trackNodesInRoute.Values.ToList());
-		WorldRenderer.RenderListOfStations(RouteManager.stationsInRoute);
+		LoadLine("roslagsbanan_27");
+		//VehicleManager.vehicleWorldCoordinate = RouteManager.trackNodesInRoute.Last().Value.WorldCoordinate;
+		//WorldRenderer.RenderListOfTrackNodes(RouteManager.trackNodesInRoute.Values.ToList());
+		//WorldRenderer.RenderListOfStations(RouteManager.stationsInRoute);
 
-		VehicleManager.Startup();
+		//VehicleManager.Startup();
 
-		worldRoot.SetProcess(true);
+		//worldRoot.SetProcess(true);
 	}
 
 	public static void Tick(double delta)
 	{
-		VehicleManager.Tick(delta);
-		WorldRenderer.RenderTick();
+		//VehicleManager.Tick(delta);
+		//WorldRenderer.RenderTick();
+	}
+
+	public static void LoadLine(string lineName)
+	{
+		var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+		var lineFolderPath = Path.Combine(path, "OpenTrainER", "lines", lineName);
+		using (StreamReader r = new StreamReader(Path.Combine(lineFolderPath, "track.json")))
+		{
+			string json = r.ReadToEnd();
+			Dictionary<ulong, TrackPoint> track = JsonConvert.DeserializeObject<Dictionary<ulong, TrackPoint>>(json);
+			//WorldRenderer.RenderTrack(track);
+		}
 	}
 }
 
--- source/WorldRoot.cs
+++ source/WorldRoot.cs
@@ -1,4 +1,7 @@
 using Godot;
+using Godot.NativeInterop;
+using System.Collections.Generic;
+
 internal partial class WorldRoot : Node
 {
 	[Export]
@@ -26,6 +29,60 @@ internal partial class WorldRoot : Node
 		{
 			WorldManager.Tick(delta);
 		}
+	}
+
+    public void RenderTrack(Dictionary<ulong, TrackPoint> trackPoints)
+    {
+        foreach (var trackPoint in trackPoints)
+        {
+            ulong pointId = trackPoint.Key;
+            TrackPoint point = trackPoint.Value;
+			foreach (var neighbour in point.linked_nodes)
+			{
+
+			}
+        }
+    }
+	
+	public Node3D InstancePoint(TrackPoint point, bool b_sprite = false)
+	{
+        Node3D node = new Node3D();
+        AddChild(node);
+        node.Position = new Vector3((float)point.xoffset, 0, (float)point.yoffset);
+
+        if (b_sprite)
+		{
+			Sprite3D sprite = new Sprite3D();
+			node.AddChild(sprite);
+			sprite.Texture = (Texture2D)GD.Load("res://icon.svg");
+			//sprite.GlobalScale(trackNode.tangentVector);
+			sprite.PixelSize = 0.02f;
+			sprite.RotateX(Mathf.Pi / 2);
+		}
+
+		return node;
+    }
+
+	public void InstanceHalfTrackSegment(TrackPoint a, TrackPoint b)
+	{
+        Vector3 aTangent = new Vector3(a.tangent[0], 0, a.tangent[1]);
+        Vector3 bTangent = new Vector3(b.tangent[0], 0, b.tangent[1]);
+		Vector3 abDisplacement = new Vector3((float)(b.xoffset - a.xoffset), 0, (float)(b.yoffset - a.yoffset));
+
+		Path3D path = new Path3D();
+		AddChild(path);
+		path.Position = new Vector3((float)a.xoffset, 0, (float)a.yoffset);
+
+		path.Curve = new Curve3D();
+        path.Curve.AddPoint(Vector3.Zero, Vector3.Zero, aTangent);
+        path.Curve.AddPoint(abDisplacement, bTangent, Vector3.Zero);
+		
+		CsgPolygon3D csgPolygon3D = new();
+		path.AddChild(csgPolygon3D);
+		csgPolygon3D.Mode = CsgPolygon3D.ModeEnum.Path;
+		csgPolygon3D.PathNode = path.GetPath();
+		Vector2[] polygonPoints = { new(0, 1), new(1, 1), new(1, 0), new(0, 0) };
+		csgPolygon3D.Polygon = polygonPoints;
 	}
 }
 
