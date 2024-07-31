01ff5700ecb26799fcdbc3e606e39e7c7dead6c3
lexag <lexa.grebnets@gmail.com>
2024-08-01 01:25:05 +0200

    DashboardGUIComponent with gauge

--- docs/Vehicle Properties.md
+++ docs/Vehicle Properties.md
@@ -1,8 +1,10 @@
 # List of vehicle properties
-| Name             | Unit   | Initializing Component | Note |
-|------------------|--------|------------------------|------|
-| controls:*       | signal | KeyboardInputComponent |      |
-| gradient         | 1      | DriverComponent        |      |
-| speed            | m/s    | DriverComponent        |      |
-| status:wheelslip | signal | DriverComponent        |      |
-| traction_force   | N      | DriverComponent        |      |
+| Name             | Unit   | Initializing Component        | Note |
+|------------------|--------|-------------------------------|------|
+| controls:*       | signal | KeyboardInputComponent        |      |
+| gradient         | 1      | DriverComponent               |      |
+| line_frequency   | Hz     | ElectricityCollectorComponent |      |
+| line_voltage     | V      | ElectricityCollectorComponent |      |
+| speed            | m/s    | DriverComponent               |      |
+| traction_force   | N      | DriverComponent               |      |
+| status:wheelslip | signal | DriverComponent               |      |
---
+++ source/vehicle/component/ElectricityCollectorComponent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTrainER.source.vehicle.component
+{
+    internal class ElectricityCollectorComponent : VehicleComponent
+    {
+        string trackPointId;
+        string prevTrackPointId;
+
+        double lineVoltage;
+        double lineFrequency;
+
+        protected override void OnInit()
+        {
+            Vehicle.InitProperty("line_voltage");
+            Vehicle.InitProperty("line_frequency");
+        }
+
+        protected override void OnTick(double delta)
+        {
+
+            if (trackPointId != Vehicle.currentRoute.points[Vehicle.routePointIndex]) 
+            {
+                prevTrackPointId = Vehicle.currentRoute.points[Vehicle.routePointIndex - 1];
+                trackPointId = Vehicle.currentRoute.points[Vehicle.routePointIndex];
+                lineVoltage = double.Parse(WorldManager.track.points[trackPointId].linked_nodes[prevTrackPointId].tags["voltage"]);
+                lineFrequency = double.Parse(WorldManager.track.points[trackPointId].linked_nodes[prevTrackPointId].tags["frequency"]);
+                
+                Vehicle.SetProperty("line_voltage", lineVoltage);
+                Vehicle.SetProperty("line_frequency", lineFrequency);
+
+            }
+        }
+    }
+}
