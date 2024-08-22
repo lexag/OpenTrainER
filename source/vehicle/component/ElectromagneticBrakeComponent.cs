2f8b6dcdcec73b744a4aa4fa8b878c33ee555d32
lexag <lexa.grebnets@gmail.com>
2024-08-22 15:01:06 +0200

    Electromagnetic Brake Component

---
+++ source/vehicle/component/ElectromagneticBrakeComponent.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTrainER.source.vehicle.component
+{
+    internal class ElectromagneticBrakeComponent : VehicleComponent
+    {
+        public double max_efficiency_speed;
+        public double max_brake_force;
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+
+        protected override void OnTick(double delta)
+        {
+            base.OnTick(delta);
+
+            double brakingForce = max_brake_force * Mathf.Min(Vehicle.GetProperty("speed") / max_efficiency_speed, 1) * Vehicle.GetProperty("controls:electromagnetic_brake");
+
+            Vehicle.ChangeProperty("braking_force", brakingForce);
+        }
+    }
+}
